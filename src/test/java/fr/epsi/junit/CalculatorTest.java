356989542975a3214e7eac38605888a0e3098636
GoroNo <67910425+GoroNo@users.noreply.github.com>
2021-06-21 12:35:59 +0200

    Update CalculatorTest.java
    fin de cour

--- src/test/java/fr/epsi/junit/CalculatorTest.java
+++ src/test/java/fr/epsi/junit/CalculatorTest.java
@@ -2,18 +2,30 @@ package fr.epsi.junit;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import java.util.Scanner;
+
 
 public class CalculatorTest {
 
     private Calculator calculator;
 
+   @BeforeEach
     public void init(){
-        //TODO
+        this.calculator = new Calculator ();
+    }
+    public void should_add_two_numbers(){
+
+         Scanner sca = new Scanner(System.in);
+         System.out.println("Veuillez saisir un nombre a :");
+         int a = sca.nextInt();
+         Scanner scb = new Scanner(System.in);
+         System.out.println("Veuillez saisir un nombre b :");
+         int b = scb.nextInt();
+
+
     }
 
-    public void should_add_two_numbers(){
-       // TODO
-    }
 
     public void should_substract_two_numbers(){
         // TODO
