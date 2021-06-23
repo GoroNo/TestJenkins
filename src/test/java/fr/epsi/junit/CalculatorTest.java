6b6140c86238b051bea0100f54b5a68555e93f6c
GoroNo <67910425+GoroNo@users.noreply.github.com>
2021-06-23 15:53:42 +0200

    new commit

--- src/test/java/fr/epsi/junit/CalculatorTest.java
+++ src/test/java/fr/epsi/junit/CalculatorTest.java
@@ -14,22 +14,50 @@ public class CalculatorTest {
     public void init(){
         this.calculator = new Calculator ();
     }
+    
+    
+    @Test
     public void should_add_two_numbers(){
-
-         Scanner sca = new Scanner(System.in);
-         System.out.println("Veuillez saisir un nombre a :");
-         int a = sca.nextInt();
-         Scanner scb = new Scanner(System.in);
-         System.out.println("Veuillez saisir un nombre b :");
-         int b = scb.nextInt();
-
-
+        int a = 3;
+        int b = 4;
+       int result = calculator.add(a,b);
+        Assertions.assertEquals(7, result);
     }
 
 
+
+     @Test
     public void should_substract_two_numbers(){
-        // TODO
+        int a = 3;
+        int b= 4;
+        int result = calculator.substract(a,b);
+        Assertions.assertEquals(-1, result);
     }
+
+    @Test
+    public void should_multi_two_numbers(){
+        int a = 3;
+        int b= 4;
+        int result = calculator.multi(a,b);
+        Assertions.assertEquals(12, result);
+    }
+
+    @Test
+    public void should_div_two_numbers(){
+        int a = 4;
+        int b= 2;
+        int result = calculator.div(a,b);
+        Assertions.assertEquals(2, result);
+    }
+
+    @Test
+    public void should_add_tab(){
+        int nmbers[] = {1,3,5,7,9};
+        int result = calculator.add(nmbers);
+        Assertions.assertEquals(25, result);
+    }
+}
+
 
 
 }
